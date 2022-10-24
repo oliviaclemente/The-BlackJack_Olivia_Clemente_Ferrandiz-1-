762123c7f33cf1b7d96ac9b317604d200eab71c2
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-10-24 16:27:00 +0000

    cc

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 from random import choice, sample
 
 carta = [chr(x) for x in range(0x1f0a1, 0x1f0af)]
+carta = {
+    chr(0x1f0a1): 11,
+    chr(0x1f0a2): 2,
+    chr(0x1f0a3): 3,
+    chr(0x1f0a4): 4,
+    chr(0x1f0a5): 5,
+    chr(0x1f0a6): 6,
+    chr(0x1f0a7): 7,
+    chr(0x1f0a8): 8,
+    chr(0x1f0a9): 9,
+    chr(0x1f0aa): 10,
+    chr(0x1f0ab): 10,
+    chr(0x1f0ad): 10,
+    chr(0x1f0ae): 10,
+}
+print("Carta: {}".format(" ".join(carta.keys())))
+print("Puntos: {}".format(list(carta.values())))
